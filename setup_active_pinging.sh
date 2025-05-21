ab0d8a680ec2098f80202c8887d73faee4bf8d18
user65434680 <s2kava01@students.osao.fi>
2025-05-21 15:47:16 +0300

    maybe fixed

--- setup_active_pinging.sh
+++ setup_active_pinging.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 SCRIPT_PATH="/opt/active_pinging/active_pinging.py"
 SERVICE_PATH="/etc/systemd/system/active_pinging.service"
 
@@ -19,14 +20,14 @@ fi
 
 cat <<EOF > "$SERVICE_PATH"
 [Unit]
-Description=Network Scan Service
+Description=Active Pinging Service
 After=network-online.target 
 Wants=network-online.target
 RequiresMountsFor=/opt/active_pinging /etc/systemd/system
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/python3 /opt/active_pinging/active_pinging.py"
+ExecStart=/usr/bin/python3 /opt/active_pinging/active_pinging.py
 RemainAfterExit=yes
 
 [Install]
@@ -35,17 +36,16 @@ EOF
 
 echo "Created systemd service file at $SERVICE_PATH."
 
-
 systemctl daemon-reexec
 systemctl daemon-reload
-systemctl enable active_pinging.service"
+systemctl enable active_pinging.service
 
-echo "Service enabled to run on boot: active_pinging.service""
+echo "Service enabled to run on boot: active_pinging.service"
 
 echo "Setting permissions..."
 sudo chmod 700 /opt/active_pinging
-sudo chmod 700 /opt/active_pinging/active_pinging.py"
-sudo chown root:root /opt/active_pinging/active_pinging.py"
+sudo chmod 700 /opt/active_pinging/active_pinging.py
+sudo chown root:root /opt/active_pinging/active_pinging.py
 
-echo "Starting network scan service..."
-sudo systemctl start active_pinging.service"
+echo "Starting active pinging service..."
+sudo systemctl start active_pinging.service
